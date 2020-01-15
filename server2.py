85aa9410cdff90e08f2a27c27fadb27c91344425
claire <clairenatacha.pillet@free.fr>
2020-01-15 17:09:07 +0100

    post ok

--- server2.py
+++ server2.py
@@ -3,8 +3,12 @@ import re
 from PIL import Image
 from io import StringIO
 import numpy as np
+from flask_cors import CORS, cross_origin
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
@@ -17,11 +21,14 @@ def capture():
 def index():
     return render_template('index.html')
 
-@app.route('/capture.html', methods=['POST'])
+@app.route('/apiCapture', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def get_image():
-    image_b64 = request.values['imageBase64']
+    image_b64 = request.values['canvas_data']
     print(image_b64)
-    image_data = re.sub('^data:image/.+;base64,', '', image_b64).decode('base64')
+
+    plt.imshow(image_b64)
+    #image_data = re.sub(image_b64).decode('base64')
     image_PIL = Image.open(StringIO(image_b64))
     image_np = np.array(image_PIL)
     print('Image received: {}'.format(image_np.shape))
